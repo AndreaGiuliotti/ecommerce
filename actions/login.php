31150564bbec7a6c61d6878e059a3bf177ec641a
AndreaGiuliotti <98811650+AndreaGiuliotti@users.noreply.github.com>
2024-05-29 12:17:52 +0200

    Update login.php

--- actions/login.php
+++ actions/login.php
@@ -15,11 +15,11 @@ if ($_GET['email']) {
 $params_user = array('email' => $email, 'password' => $password);
 $user = User::Find($params_user);
 if (!$user) {
-    header('Location: http://localhost:63342/ecommerce/views/signup.php');
+    header('Location: http://localhost/ecommerce/views/signup.php');
 } else {
     $_SESSION['current_user'] = $user;
     $params = array('ip' => $_SERVER['REMOTE_ADDR'], 'data_login' => date('Y-m-d H:i:s'), 'user_id' => User::Find($params_user)->getId());
     Session::Create($params);
-    header('Location: http://localhost:63342/ecommerce/views/products/index.php');
+    header('Location: http://localhost/ecommerce/views/products/index.php');
 }
 exit;
