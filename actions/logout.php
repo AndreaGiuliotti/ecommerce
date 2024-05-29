8d0e56b189e7e3df80686dd8989bc0fdb17306b4
AndreaGiuliotti <98811650+AndreaGiuliotti@users.noreply.github.com>
2024-05-29 12:07:57 +0200

    update url

--- actions/logout.php
+++ actions/logout.php
@@ -3,4 +3,4 @@ require_once '../models/User.php';
 
 session_start();
 $_SESSION = null;
-header('Location: http://localhost:63342/E-Commerce_V2/views/login.php');
+header('Location: http://localhost:63342/ecommerce/views/login.php');
