5fa1fc2a2d0f1d16f66add9fc75020ae8bfcdff8
AndreaGiuliotti <98811650+AndreaGiuliotti@users.noreply.github.com>
2024-05-29 12:17:37 +0200

    Update edit_cart.php

--- actions/edit_cart.php
+++ actions/edit_cart.php
@@ -22,5 +22,5 @@ if ($quantita > 0) {
     $itemline->delete();
 }
 
-header('Location: http://localhost:63342/ecommerce/views/products/view_cart.php');
+header('Location: http://localhost/ecommerce/views/products/view_cart.php');
 exit;
